6e541df3213e03ce5aa149285fb8029de3f4f05a
Alexander Chursin <37036721+AlexaDevel@users.noreply.github.com>
2021-12-01 14:46:11 +0300

    Update utils

--- utils.py
+++ utils.py
@@ -8,8 +8,11 @@ pattern = re.compile(r'((?:\+\d{2}[-\.\s]??|\d{4}[-\.\s]??)?(?:\d{3}[-\.\s]??\d{
 def is_number(text: str) -> bool:
     return pattern.match(text) is not None
 
-def get_birthday(text: str) -> Optional[datetime]:
+def get_birthday(text: str) -> Optional[str]:
     try:
-        return datetime.strptime(text, "%d.%m.%Y")
+        int_v = int(text)
+        if 5 < int_v < 120:
+            return str(int_v)
     except:
         return None
+    return None
