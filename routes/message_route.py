ba4c2dea05422712c6d9b4099a84427740dad1fd
Alexander Marsin <aleksandr26rus25@gmail.com>
2021-12-22 13:20:23 +0300

    fix tg route

--- routes/message_route.py
+++ routes/message_route.py
@@ -2,53 +2,61 @@ from typing import Optional
 
 from fastapi import File, APIRouter, Query
 from fastapi.responses import JSONResponse
-from pytrovich.maker import PetrovichDeclinationMaker
 
 from messenger_api import mess_api
-from .schemas import *
 from telegram.aiogram_view import tg_view
+from .schemas import *
 from .service import finish_consulate
 
 message_route = APIRouter()
 util_route = APIRouter()
 
 
+class DialogMessage(BaseModel):
+    dialog_id: int
+    text: str
+    doctor_name: str
+
+
+class DialogFile(BaseModel):
+    dialog_id: int
+    filename: str
+    doctor_name: str
+    body: bytes = File(...)
+
+
+class FinishMessage(BaseModel):
+    dialog_id: int
+    text: str
+    doctor_name: Optional[str] = Query(None, description='Имя доктора в падеже. Если добавить это поле, то сообщение будет отправлено от имени ассистента')
+
+
 @message_route.post("/send/text_message", response_model=Message, responses={404: {"model": ErrorMessage}})
-async def text_message(dialog_id: int, text: str, doctor_name: str):
-    consulate = await mess_api.get_consulate(dialog_id)
+async def text_message(dialog_message: DialogMessage):
+    consulate = await mess_api.get_consulate(dialog_message.dialog_id)
     if consulate is not None:
-        await tg_view.send_message_doctor(chat_id=consulate.chat_id, text=text, doctor_name=doctor_name)
+        await tg_view.send_message_doctor(chat_id=consulate.chat_id, text=dialog_message.text, doctor_name=dialog_message.doctor_name)
         return Message(chat_id=consulate.chat_id)
     else:
         return JSONResponse(status_code=404, content={'detail': 'Consulate not found'})
 
 
 @message_route.post("/send/finish_message", response_model=Message, responses={404: {"model": ErrorMessage}})
-async def finish_message(dialog_id: int, text: str, doctor_name: Optional[str] = Query(None, description='Имя доктора. Если добавить это поле, то сообщение будет отправлено от имени ассистента')):
-    chat_id = await finish_consulate(dialog_id, text, doctor_name)
+async def finish_message(dialog_message: FinishMessage):
+    chat_id = await finish_consulate(dialog_message.dialog_id, dialog_message.text, dialog_message.doctor_name)
     if chat_id:
         return Message(chat_id=chat_id)
     else:
         return JSONResponse(status_code=404, content={'detail': 'Consulate not found'})
 
 
-@message_route.post("/command/create_consulate", deprecated=True)
-async def create_consulate(dialog_id: int, chat_id: int):
-    id_ = await mess_api.set_client(chat_id, dialog_id)
-    await tg_view.send_assistant_message(chat_id, 'создана запись с консультацией')
-    return {"id": id_, }
-
-
 @message_route.post("/send/file_message", response_model=Message, responses={404: {"model": ErrorMessage}})
-async def file_message(dialog_id: int, filename: str, doctor_name: str, body: bytes = File(...)):
-    chat_id = mess_api.get_chat_id(dialog_id=dialog_id)
-    if chat_id is not None:
-        await tg_view.send_file_from_doctor(chat_id, data=body, filename=filename, doctor_name=doctor_name)
-        return Message(chat_id=chat_id)
+async def file_message(dialog_file: DialogFile):
+    consulate = await mess_api.get_consulate(dialog_file.dialog_id)
+    if consulate is not None:
+        await tg_view.send_file_from_doctor(consulate.chat_id, data=dialog_file.body, filename=dialog_file.filename, doctor_name=dialog_file.doctor_name)
+        return Message(chat_id=consulate.chat_id)
     else:
         return JSONResponse(status_code=404, content={'detail': 'Consulate not found'})
-
-
-maker = PetrovichDeclinationMaker()
 
 
